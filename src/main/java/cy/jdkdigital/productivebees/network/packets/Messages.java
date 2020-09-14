c0da318113880236109ec5a9e91289fee1431dd0
Jais Djurhuus-Kempel <inquiry@jdkdigital.cy>
2020-09-14 20:21:22 +0300

    Switch to play packages instead of login packages.

--- src/main/java/cy/jdkdigital/productivebees/network/packets/Messages.java
+++ src/main/java/cy/jdkdigital/productivebees/network/packets/Messages.java
@@ -1,7 +1,7 @@
 package cy.jdkdigital.productivebees.network.packets;
 
-import cy.jdkdigital.productivebees.ProductiveBees;
 import cy.jdkdigital.productivebees.setup.BeeReloadListener;
+import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
@@ -9,31 +9,12 @@ import net.minecraftforge.fml.network.NetworkEvent;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class Messages
 {
-    static class LoginIndexedMessage implements IntSupplier
-    {
-        private int loginIndex;
-
-        public void setLoginIndex(final int loginIndex) {
-            this.loginIndex = loginIndex;
-        }
-
-        public int getLoginIndex() {
-            return loginIndex;
-        }
-
-        @Override
-        public int getAsInt() {
-            return getLoginIndex();
-        }
-    }
-
-    public static class BeesMessage extends LoginIndexedMessage
+    public static class BeesMessage
     {
         public Map<ResourceLocation, CompoundNBT> data;
 
@@ -42,7 +23,6 @@ public class Messages
         }
 
         public static void encode(BeesMessage message, PacketBuffer buffer) {
-            ProductiveBees.LOGGER.info("encode BeesMessage " + buffer);
             buffer.writeInt(message.data.size());
             for (Map.Entry<ResourceLocation, CompoundNBT> entry : message.data.entrySet()) {
                 buffer.writeResourceLocation(entry.getKey());
@@ -51,7 +31,6 @@ public class Messages
         }
 
         public static BeesMessage decode(PacketBuffer buffer) {
-            ProductiveBees.LOGGER.info("decode BeesMessage " + buffer);
             Map<ResourceLocation, CompoundNBT> data = new HashMap<>();
             IntStream.range(0, buffer.readInt()).forEach(i -> {
                 data.put(buffer.readResourceLocation(), buffer.readCompoundTag());
@@ -60,9 +39,10 @@ public class Messages
         }
 
         public static void handle(BeesMessage message, Supplier<NetworkEvent.Context> context) {
-            ProductiveBees.LOGGER.info("Handled BeesMessage " + message);
             context.get().enqueueWork(() -> {
                 BeeReloadListener.INSTANCE.setData(message.data);
+                // Trigger jei reload
+                net.minecraftforge.client.ForgeHooksClient.onRecipesUpdated(new RecipeManager());
             });
             context.get().setPacketHandled(true);
         }
