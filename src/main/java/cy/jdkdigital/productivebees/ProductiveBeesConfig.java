bbe66f0dcb50e1fd68c79be81da5f9886bb1b876
Jais Djurhuus-Kempel <inquiry@jdkdigital.cy>
2020-11-16 18:53:07 +0200

    Fixed Mekanism dependency

--- src/main/java/cy/jdkdigital/productivebees/ProductiveBeesConfig.java
+++ src/main/java/cy/jdkdigital/productivebees/ProductiveBeesConfig.java
@@ -60,7 +60,7 @@ public class ProductiveBeesConfig
                     .defineInRange("centrifugeProcessingTime", 300, 20, Integer.MAX_VALUE);
 
             centrifugePoweredProcessingTime = builder
-                    .comment("How many ticks it takes for process a recipe in the powerd centrifuge. Default 100.")
+                    .comment("How many ticks it takes for process a recipe in the powered centrifuge. Default 100.")
                     .defineInRange("centrifugePoweredProcessingTime", 100, 20, Integer.MAX_VALUE);
 
             centrifugePowerUse = builder
