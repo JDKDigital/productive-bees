878a9945e3e5c11b1f01c768d1e6d5eb6116dc82
Jais Djurhuus-Kempel <inquiry@jdkdigital.cy>
2020-12-02 23:55:22 +0200

    Fix BZ xrash

---
+++ src/main/java/cy/jdkdigital/productivebees/block/ExpansionBox.java
@@ -0,0 +1,8 @@
+package cy.jdkdigital.productivebees.block;
+
+public class ExpansionBox extends cy.jdkdigital.productivebees.common.block.ExpansionBox
+{
+    public ExpansionBox(Properties properties) {
+        super(properties);
+    }
+}
