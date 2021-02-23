377b07f46e96dab89fb0c87313444f8a60ceeb9c
Jais Djurhuus-Kempel <inquiry@jdkdigital.cy>
2021-02-23 22:42:12 +0200

    Fix solitary egg incubator recipe

--- src/main/java/cy/jdkdigital/productivebees/init/ModItemGroups.java
+++ src/main/java/cy/jdkdigital/productivebees/init/ModItemGroups.java
@@ -7,6 +7,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
@@ -64,8 +66,14 @@ public class ModItemGroups
         }
 
         public static ItemStack getSpawnEgg(String beeType) {
-            ItemStack egg = new ItemStack(ModItems.CONFIGURABLE_SPAWN_EGG.get());
-            setTag(beeType, egg);
+            ItemStack egg;
+            if (BeeReloadListener.INSTANCE.getData(beeType) != null) {
+                egg = new ItemStack(ModItems.CONFIGURABLE_SPAWN_EGG.get());
+                setTag(beeType, egg);
+            } else {
+                String name = beeType.contains(":") ? beeType.split("[:]")[1] : beeType;
+                egg = new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(ProductiveBees.MODID, "spawn_egg_" + name)));
+            }
             return egg;
         }
     }
