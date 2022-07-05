5cdf8585df4010f22702371fb00a526c2a03d5ec
Jais Djurhuus-Kempel <jdk@combine.dk>
2022-07-05 18:58:18 +0300

    cleanup

--- src/main/java/cy/jdkdigital/productivebees/common/item/BeeCage.java
+++ src/main/java/cy/jdkdigital/productivebees/common/item/BeeCage.java
@@ -29,7 +29,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
 import net.minecraftforge.fml.ModList;
 
 import javax.annotation.Nonnull;
